8faf8395d0581a4623e24f981b381aa874df7713
mrhaffner <mrhaffner13@gmail.com>
2022-05-07 13:53:12 -0500

    refactor: added type annotations and doc strings

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,28 +1,39 @@
 import pandas as pd
-import pathlib
 from bs4 import BeautifulSoup
-from utilities.article import article_factory
+from pathlib import Path
+from typing import List
+from utilities.article import Article, article_factory
 from utilities.pathing import get_path_with_current_datetime
 
-# takes in a datetime
-# finds the appropriate folder at that datetime
-# for each xml file in that folder, parses the xml rss file
-# saves title, url, pubdate, and author? for each article in feed to db/or file? with specified datetime/website
-# key can be url? or title+website?
-
-# function that creates Article data class from 
 
 # throws i/o error
-def save_parsed_articles(articles, publisher):
+def save_parsed_articles(articles: List[Article], publisher: str) -> None:
+    """
+        Saves an array of articles to a parquet file
+
+        An array of Articles is converted to a pandas DataFrame to prepare for saving
+
+        The following directory is created and the parquet file is saved to:
+
+        /in_the_news/data/parsed/year/day/month/hour/publisher.parquet
+
+        /year/day/month/hour/ comes from when the current time, which is when the article was scraped
+    """
     # maybe get the path to save from based on input date folders from scraped folder
     dir_to_save = get_path_with_current_datetime('in_the_news', 'data/parsed')
     dir_to_save.mkdir(parents=True, exist_ok=True)
+    # pandas may be unneccesary, might be better to directly use pyarrow
     df = pd.DataFrame([article for article in articles])
-    df.to_parquet(pathlib.Path.joinpath(dir_to_save, publisher + '.parquet'))
+    df.to_parquet(Path.joinpath(dir_to_save, publisher + '.parquet'))
 
 
 # throws i/o error
-def parse_articles_from_soup(soup, publisher):
+def parse_articles_from_soup(soup: BeautifulSoup, publisher) -> List[Article]:
+    """
+        Parses all articles from an RSS feeds into a list of Article objects and outputs that list
+
+        RSS feed is input as a BeautifulSoup and and the publisher is added to the Article
+    """
     items = soup.find_all('item')
 
     articles = []
@@ -35,7 +46,8 @@ def parse_articles_from_soup(soup, publisher):
 
 
 # throws i/o error
-def parse_scraped_data(directory):
+def parse_scraped_data(directory: Path) -> None:
+    """Parses all .xml files in a given directory and saves them to a seperate directory as .parquet files"""
     for file in directory:
         if str(file).endswith('xml'):
             with open(file) as f:
@@ -46,5 +58,5 @@ def parse_scraped_data(directory):
 
 if __name__ == "__main__":
     # this should find most recently scraped folder or generate a path based on datetime input
-    dir_to_parse = pathlib.Path(__file__).parent.parent.joinpath('data/scraped/2022/05/07/17')
+    dir_to_parse = Path(__file__).parent.parent.joinpath('data/scraped/2022/05/07/17')
     parse_scraped_data(dir_to_parse.glob('*'))
