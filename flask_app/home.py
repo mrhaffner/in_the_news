c83bbacff36dcac2524faa2442e40747ef266cd1
mrhaffner <mrhaffner13@gmail.com>
2022-05-13 22:00:24 -0500

    feat: recent trend in sentiment is now calculated and displayed

--- flask_app/home.py
+++ flask_app/home.py
@@ -34,29 +34,55 @@ def float_dict_to_percent(dict):
     return dict
 
 
-def get_latest_sentiment():
+def get_second_latest_sentiment():
     sql_rows = (
         get_db()
         .execute(
             "SELECT left_mean_sentiment, right_mean_sentiment, all_mean_sentiment"
             " FROM Sentiment"
-            " ORDER BY datetime DESC LIMIT 1",
+            " ORDER BY datetime DESC LIMIT 2",
         )
-        .fetchone()
+        .fetchall()
     )
 
     if sql_rows is None:
         abort(404, "No sentiment record found!")
 
-    sentiment = {'left_mean_sentiment': sql_rows['left_mean_sentiment'], 'right_mean_sentiment': sql_rows['right_mean_sentiment'], 'all_mean_sentiment': sql_rows['all_mean_sentiment']}
+    # index 0 is the most recent
+    print(sql_rows_to_list(sql_rows))
+    return sql_rows_to_list(sql_rows)
 
-    return sentiment
+
+def sql_row_to_dict(sql):
+    return {'left_mean_sentiment': float(sql['left_mean_sentiment']), 'right_mean_sentiment': float(sql['right_mean_sentiment']), 'all_mean_sentiment': float(sql['all_mean_sentiment'])}    
+
+
+def sql_rows_to_list(sql):
+    return [sql_row_to_dict(s) for s in sql]
+
+
+def get_trend(most_recent, second_recent):
+    if most_recent > second_recent:
+        return "up"
+    elif most_recent < second_recent:
+        return "down"
+    else:
+        return "none"
+
+
+def get_sentiment_trend(sentiments):
+    trend = {}
+    trend['left'] = get_trend(sentiments[0]['left_mean_sentiment'], sentiments[1]['left_mean_sentiment'])
+    trend['all'] = get_trend(sentiments[0]['all_mean_sentiment'], sentiments[1]['all_mean_sentiment'])
+    trend['right'] = get_trend(sentiments[0]['right_mean_sentiment'], sentiments[1]['right_mean_sentiment'])
+    return trend
 
 
 @bp.route("/")
 def index():
-    sentiment = get_latest_sentiment()
-    moods = get_moods_from_sentiment(sentiment)
-    perc_sentiment = float_dict_to_percent(sentiment)
+    sentiments = get_second_latest_sentiment()
+    moods = get_moods_from_sentiment(sentiments[0])
+    perc_sentiment = float_dict_to_percent(sentiments[0])
+    trend = get_sentiment_trend(sentiments)
 
-    return render_template("sentiment/index.html", sentiment=perc_sentiment, moods=moods)
+    return render_template("sentiment/index.html", sentiment=perc_sentiment, moods=moods, trend=trend)
