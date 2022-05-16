1af5a4859485a88bb8a9961fb8279e6cf2640680
mrhaffner <mrhaffner13@gmail.com>
2022-05-16 14:21:00 -0500

    refactor: cleaned up scraper code

--- dags/scripts/scraper.py
+++ dags/scripts/scraper.py
@@ -1,9 +1,9 @@
 import json
-import pathlib
+import random
 import requests
-from scripts.utilities.data_struct import RotatingList
-from scripts.utilities.files import csv_to_dict
 from airflow.models import Variable
+from pathlib import Path
+from scripts.utilities.files import csv_to_dict
 from scripts.utilities.pathing import add_datetime_to_path
 from typing import TypedDict
 
@@ -14,52 +14,32 @@ class Website(TypedDict):
     rss_url: str
 
 
-def _scrape_websites_to_xml(rss_websites: Website, parent_dir: pathlib.Path) -> None:
-    """
-    Scrapes a list of rss_websites, saves each website to an xml file in parent_dir
-
-    Throws IOError???
-    """
-    # move set up to another function or maybe created a scraped class to handle setup?
-    json_path = pathlib.Path(__file__).parent.joinpath('config/user_agents.json')
-    # needs error handling
+def _get_user_agent() -> str:
+    """Returns a random user_agent string from configuration file."""
+    json_path = Path(__file__).parent.joinpath('config/user_agents.json')
     with open(json_path) as f:
         user_agents = json.load(f)
+    return user_agents[random.randint(0, len(user_agents) - 1)]
 
+
+def _scrape_websites_to_xml(rss_websites: Website, parent_dir: Path) -> None:
+    """Scrapes a list of rss_websites, saves each website to an xml file in parent_dir."""
+    headers = {'User-agent': _get_user_agent()}
     parent_dir.mkdir(parents=True, exist_ok=True)
-    headers_obj = RotatingList(user_agents) # rename
     
     for website in rss_websites:
-        headers = headers_obj.next # rename
-        
-        # proxy + rotate proxy
-        
-        request = requests.get(website['rss_url'], headers) # thought i needed to add {user-agent: ...} ????
+        request = requests.get(website['rss_url'], headers = headers)
         xml = request.text
-        
-    #   check if html or xml for error/saving?
-        with open(pathlib.Path(parent_dir, f"{website['id']}.xml"), 'w') as f:
+        with open(Path(parent_dir, f"{website['id']}.xml"), 'w') as f:
             f.write(xml)
 
             
-def scraper():
-    csv_path = pathlib.Path(__file__).parent.joinpath('config/news_sites.csv')
+def scraper() -> None:
+    """Scrapes a list of websites from config csv file and saves them as xml files."""
+    csv_path = Path(__file__).parent.joinpath('config/news_sites.csv')
     rss_websites = csv_to_dict(csv_path)
 
-    parsed_dir_root = pathlib.Path(Variable.get('base_dir')).joinpath('data/scraped')
+    parsed_dir_root = Path(Variable.get('base_dir')).joinpath('data/scraped')
     path_to_save = add_datetime_to_path(parsed_dir_root, Variable.get('current_time'))
 
-    #path_to_save = get_path_with_current_datetime('in_the_news', 'data/scraped')
-
     _scrape_websites_to_xml(rss_websites, path_to_save)
-
-
-# use config file for path to save scraped websites
-# error handling?
-# tests?
-# async
-# federalist returning forbidden ?
-# rescrape with different header / proxy if scrape fails?
-# docstrings
-# utilities modules...
-# logs? maybe at airflow level?
