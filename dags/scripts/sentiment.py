104d6b8aa4a05f13806cfdf83ca7446754bd6d2d
mrhaffner <mrhaffner13@gmail.com>
2022-05-31 14:07:08 -0500

    refactor: moved more dataframe creation methods to utilities from sentimentizer

--- dags/scripts/sentiment.py
+++ dags/scripts/sentiment.py
@@ -1,48 +1,17 @@
 import datetime
-import pandas as pd
 
 from pathlib import Path
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from airflow.models import Variable
 from airflow.providers.sqlite.hooks.sqlite import SqliteHook
-from scripts.utilities.dataframe import classify_leaning, get_articles_df_from_path 
+from scripts.utilities.dataframe import create_sentiment_df
 from scripts.utilities.pathing import add_datetime_to_path
 
 
-def _get_sentiment(text: str) -> float:
-    '''
-    Returns the sentiment score for a given String. Score ranges from -1 to 1.
-    Positive scores indicate positive sentiment.
-    Negative scores indicate negative sentiment.
-    Neutral sentiment is indicated by 0.
-    '''
-    sia = SentimentIntensityAnalyzer()
-    return sia.polarity_scores(text)['compound']
-
-
-def _classify_sentiment(articles_df: pd.DataFrame) -> pd.DataFrame:
-    '''
-    Adds sentiment and political leaning columns to a dataframe
-
-    articles_df needs columns:
-        title: str (sentiment will be analyzed)
-        publisher: str (to get political leaning)
-
-    output dataframe will have new columns:
-        sentiment: float (range from -1 to 1)
-        leaning: str (values 'right' or 'left' representing political leaning)
-    '''
-    articles_df['sentiment'] = articles_df['title'].apply(_get_sentiment)
-    articles_df['leaning'] = articles_df['publisher'].apply(classify_leaning)
-    return articles_df
-
-
 def sentimentizer() -> None:
-    '''Gathers means sentiment data from all, left leaning, and right leaning media and saves to database.'''
+    '''Gathers mean sentiment data from all, left leaning, and right leaning media and saves to database.'''
     parsed_dir_root = Path(Variable.get('base_dir')).joinpath('data/parsed')
     parsed_dir = add_datetime_to_path(parsed_dir_root, Variable.get('current_time'))
-    articles_df = get_articles_df_from_path(parsed_dir)
-    classified_df = _classify_sentiment(articles_df)
+    classified_df = create_sentiment_df(parsed_dir)
     leaning_mean_df = classified_df.groupby('leaning').mean()
 
     sqlite_hook = SqliteHook(sqlite_conn_id='news_db')
--- dags/scripts/utilities/dataframe.py
+++ dags/scripts/utilities/dataframe.py
@@ -1,12 +1,14 @@
 import pandas as pd
+
 from pathlib import Path
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
 def get_articles_df_from_path(dir: Path) -> pd.DataFrame:
     '''
     Creates a dataframe containing a list of news articles from a path containing multiple Parquet files
 
-    Ouput dataframe will have colums:
+    Ouput dataframe will have columns:
         title: str (article title)
         pub_date: str (date article was published)
         url: str (link to article)
@@ -18,7 +20,7 @@ def get_articles_df_from_path(dir: Path) -> pd.DataFrame:
     return articles_df
 
 
-def classify_leaning(publisher: str) -> str:
+def _classify_leaning(publisher: str) -> str:
     '''Returns the political leaning of a given publisher'''
     csv_path = Path(__file__).parent.joinpath('config/news_sites.csv')
     df = pd.read_csv(csv_path)
@@ -28,3 +30,50 @@ def classify_leaning(publisher: str) -> str:
         return leaning[0]
     else:
         return ''
+
+
+def _get_sentiment(text: str) -> float:
+    '''
+    Returns the sentiment score for a given String. Score ranges from -1 to 1.
+    Positive scores indicate positive sentiment.
+    Negative scores indicate negative sentiment.
+    Neutral sentiment is indicated by 0.
+    '''
+    sia = SentimentIntensityAnalyzer()
+    return sia.polarity_scores(text)['compound']
+
+
+def classify_sentiment(articles_df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Adds sentiment and political leaning columns to a dataframe
+
+    articles_df needs columns:
+        title: str (sentiment will be analyzed)
+        publisher: str (to get political leaning)
+
+    output dataframe will have new columns:
+        sentiment: float (range from -1 to 1)
+        leaning: str (values 'right' or 'left' representing political leaning)
+    '''
+    articles_df['sentiment'] = articles_df['title'].apply(_get_sentiment)
+    articles_df['leaning'] = articles_df['publisher'].apply(_classify_leaning)
+    return articles_df
+
+
+def create_sentiment_df(dir: Path) -> pd.DataFrame:
+    '''
+    Creates a dataframe of news articles from parquet files in dir. Adds 
+    sentiment scores and political leaning to dataframe.
+
+    Ouput dataframe will have columsn:
+        title: str (article title)
+        pub_date: str (date article was published)
+        url: str (link to article)
+        author: str
+        publisher: str
+        sentiment: float (range from -1 to 1)
+        leaning: str (values 'right' or 'left' representing political leaning)
+    '''
+    articles_df = get_articles_df_from_path(dir)
+    classified_df = classify_sentiment(articles_df)
+    return classified_df
